e529c466d96e04df0ac452b8434a0d9ffe6149b0
Nate <nathanael.frappart@ryzhom.io>
2024-06-18 01:49:04 +0200

    first full version - not tested yet

--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
+data "azurerm_client_config" "current" {}
+
 data "azurerm_resource_group" "automation" {
-  count    = var.use_existing_rg == true ? 1 : 0
-  name     = var.rg_name
-  location = var.location
+  count = var.use_existing_rg == true ? 1 : 0
+  name  = var.aa_rg
 }
 
 resource "azurerm_resource_group" "automation" {
   count    = var.use_existing_rg == true ? 0 : 1
-  name     = var.rg_name
+  name     = var.aa_rg
   location = var.location
   tags     = var.rg_tags
 }
@@ -14,13 +15,13 @@ resource "azurerm_resource_group" "automation" {
 data "azurerm_automation_account" "automation" {
   count               = var.use_existing_aa == true ? 1 : 0
   name                = var.aa_name
-  resource_group_name = var.aa_rg
+  resource_group_name = var.use_existing_rg == true ? data.azurerm_resource_group.automation[0].name : azurerm_resource_group.automation[0].name
 }
 
 resource "azurerm_automation_account" "automation" {
   count               = var.use_existing_aa == true ? 0 : 1
   name                = var.aa_name
-  resource_group_name = var.use_existing_rg == true ? data.azurerm_resource_group.automation.name : azurerm_resource_group.automation.name
+  resource_group_name = var.use_existing_rg == true ? data.azurerm_resource_group.automation[0].name : azurerm_resource_group.automation[0].name
   location            = var.location
   identity {
     type = "SystemAssigned"
@@ -32,36 +33,56 @@ resource "azurerm_automation_account" "automation" {
 }
 
 resource "azurerm_automation_schedule" "bimonthly" {
-  count                   = var.use_existing_sechedule == true ? 0 : 1
-  name                    = "bimonthly"
-  resource_group_name     = var.use_existing_rg == true ? data.azurerm_resource_group.automation.name : azurerm_resource_group.automation.name
-  automation_account_name = var.use_existing_aa == true ? data.azurerm_automation_account.automation.name : azurerm_automation_account.automation.name
-  frequency               = "Month"
-  interval                = 2
-  timezone                = var.aa_schedule_timezone #"Europe/Paris"
-  description             = "Schedule to run every other month."
+  name                    = var.schedule_name
+  resource_group_name     = var.use_existing_rg == true ? data.azurerm_resource_group.automation[0].name : azurerm_resource_group.automation[0].name
+  automation_account_name = var.use_existing_aa == true ? data.azurerm_automation_account.automation[0].name : azurerm_automation_account.automation[0].name
+  frequency               = var.schedule_frequency
+  interval                = var.schedule_interval
+  timezone                = var.schedule_timezone
+  description             = var.schedule_description
 }
 
 
 resource "azurerm_automation_powershell72_module" "posh_acme" {
   name                  = "Posh-ACME"
-  automation_account_id = azurerm_automation_account.automation.id
+  automation_account_id = azurerm_automation_account.automation[0].id
 
   module_link {
-    uri = "https://www.powershellgallery.com/api/v2/package/Posh-ACME/4.22.0"
+    uri = "https://www.powershellgallery.com/api/v2/package/Posh-ACME/${var.posh_acme_version}"
   }
 }
 
+data "azurerm_key_vault" "kv" {
+  count               = var.use_existing_kv == true ? 1 : 0
+  name                = var.kv_name
+  resource_group_name = var.kv_rg
+}
+
+resource "azurerm_key_vault" "kv" {
+  count                       = var.use_existing_kv == true ? 0 : 1
+  name                        = var.kv_name
+  location                    = var.location
+  resource_group_name         = var.use_existing_rg == true ? data.azurerm_resource_group.automation[0].name : azurerm_resource_group.automation[0].name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = var.kv_delete_retention
+  purge_protection_enabled    = var.kv_purge_protection
+
+  sku_name = var.kv_sku
+}
+
 resource "random_password" "posh_acme" {
+  count            = var.use_default_pwd == true ? 0 : 1
   length           = 16
   special          = true
   override_special = "@!-+[]"
 }
 
 resource "azurerm_key_vault_secret" "posh_acme" {
+  count        = var.use_default_pwd == true ? 0 : 1
   name         = "posh-acme"
-  value        = random_password.posh_acme.result
-  key_vault_id = azurerm_key_vault.kv.id
+  value        = random_password.posh_acme[0].result
+  key_vault_id = var.use_existing_kv == true ? data.azurerm_key_vault.kv[0].id : azurerm_key_vault.kv[0].id
 
   lifecycle {
     ignore_changes = [value]
@@ -71,7 +92,7 @@ resource "azurerm_key_vault_secret" "posh_acme" {
 #  name                    = "letsencrypt_www"
 #  resource_group_name = azurerm_resource_group.rg["rg-core"].name
 #  location            = azurerm_resource_group.rg["rg-core"].location
-#  automation_account_name = azurerm_automation_account.automation.name
+#  automation_account_name = azurerm_automation_account.automation[0].name
 #  log_verbose             = "false" #"true"
 #  log_progress            = "true"
 #  description             = "Powershell script using posh-acme to generate letsencrypt certificates"
@@ -83,52 +104,18 @@ resource "azurerm_key_vault_secret" "posh_acme" {
 #
 #resource "azurerm_automation_job_schedule" "letsencrypt" {
 #  resource_group_name = azurerm_resource_group.rg["rg-core"].name
-#  automation_account_name = azurerm_automation_account.automation.name
+#  automation_account_name = azurerm_automation_account.automation[0].name
 #  schedule_name           = azurerm_automation_schedule.bimonthly.name
 #  runbook_name            = azurerm_automation_runbook.letsencrypt.name
 #}
 
-locals {
-  certs = {
-    vault = {
-      ca      = "LE_PROD"
-      cn      = "'vault.priv.ryzhom.com'"
-      sans    = "'vault1.priv.ryzhom.com','vault2.priv.ryzhom.com','vault3.priv.ryzhom.com'"
-      contact = "certbot@ryzhom.io"
-    },
-    consul = {
-      ca      = "LE_PROD"
-      cn      = "'consul.priv.ryzhom.com'"
-      sans    = "'consul1.priv.ryzhom.com','consul2.priv.ryzhom.com','consul3.priv.ryzhom.com'"
-      contact = "certbot@ryzhom.io"
-    },
-    www = {
-      ca      = "LE_PROD"
-      cn      = "'www.ryzhom.com'"
-      sans    = "'ryzhom.com','web.ryzhom.com'"
-      contact = "certbot@ryzhom.io"
-    },
-    aks-nginx-demo = {
-      ca      = "LE_PROD"
-      cn      = "'aks-nginx-demo.ryzhom.com'"
-      sans    = "'nginx-demo.ryzhom.com'"
-      contact = "certbot@ryzhom.io"
-    },
-    whoami = {
-      ca      = "LE_PROD"
-      cn      = "'whoami.ryzhom.com'"
-      sans    = "'aks-whoami.ryzhom.com'"
-      contact = "certbot@ryzhom.io"
-    }
-  }
-}
 
 resource "azurerm_automation_runbook" "acme_le" {
-  for_each                = local.certs
+  for_each                = var.certs
   name                    = "letsencrypt_${each.key}"
-  resource_group_name     = azurerm_resource_group.rg["rg-core"].name
-  location                = azurerm_resource_group.rg["rg-core"].location
-  automation_account_name = azurerm_automation_account.automation.name
+  resource_group_name     = var.use_existing_rg == true ? data.azurerm_resource_group.automation[0].name : azurerm_resource_group.automation[0].name
+  location                = var.location
+  automation_account_name = azurerm_automation_account.automation[0].name
   log_verbose             = "false" #"true"
   log_progress            = "true"
   description             = "Powershell script using posh-acme to generate letsencrypt certificates"
@@ -140,14 +127,20 @@ resource "azurerm_automation_runbook" "acme_le" {
     cn          = each.value.cn,
     sans        = each.value.sans,
     contact     = each.value.contact,
-    vault_name  = azurerm_key_vault.kv.name,
+    vault_name  = var.use_existing_kv == true ? data.azurerm_key_vault.kv[0].name : azurerm_key_vault.kv[0].name,
     secret_name = each.key
   })
 }
 
+data "azurerm_dns_zone" "automation_dns" {
+  for_each            = var.certs
+  name                = each.value.dns_zone_name
+  resource_group_name = each.value.dns_zone_rg
+}
 resource "azurerm_role_assignment" "automation_dns" {
-  scope                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
+  for_each             = var.certs
+  scope                = data.azurerm_dns_zone.automation_dns[each.key].id
   role_definition_name = "DNS Zone Contributor"
-  principal_id         = azurerm_automation_account.automation.identity[0].principal_id
+  principal_id         = azurerm_automation_account.automation[0].identity[0].principal_id
 }
 
--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "use_existing_rg" {
   default     = false
 }
 
-variable "rg_name" {
+variable "aa_rg" {
   description = "Name for the resource group, either existing or to be created"
   type        = string
 }
@@ -42,8 +42,92 @@ variable "aa_tags" {
   default     = {}
 }
 
-variable "use_existing_schedule" {
+variable "schedule_name" {
+  description = ""
+  type        = string
+  default     = "bimonthly"
+}
+
+variable "schedule_timezone" {
+  description = ""
+  type        = string
+  default     = "Europe/Paris"
+}
+
+variable "schedule_frequency" {
+  description = ""
+  type        = string
+  default     = "Month"
+}
+
+variable "schedule_interval" {
+  description = ""
+  type        = number
+  default     = 2
+}
+
+variable "schedule_description" {
+  description = ""
+  type        = string
+  default     = "Schedule to run every other month."
+}
+
+variable "posh_acme_version" {
+  description = ""
+  type        = string
+  default     = "4.22.0"
+}
+
+variable "use_existing_kv" {
   description = ""
   type        = bool
   default     = false
 }
+
+variable "kv_name" {
+  description = "Name of the Keyvault, either existing one or to be created by module"
+  type        = string
+}
+
+variable "kv_rg" {
+  description = "Resource Group name of the existing Keyvault, if using one. This value will be ignored if Keyvault is created by the module (using var.aa_rg value instead)"
+  type        = string
+  default     = ""
+}
+
+variable "kv_delete_retention" {
+  description = ""
+  type        = number
+  default     = 7
+}
+
+variable "kv_purge_protection" {
+  description = ""
+  type        = bool
+  default     = true
+}
+
+variable "kv_sku" {
+  description = ""
+  type        = string
+  default     = "standard"
+}
+
+variable "use_default_pwd" {
+  description = ""
+  type        = bool
+  default     = true
+}
+
+variable "certs" {
+  description = "object collection for your certificates"
+  type = map(object({
+    ca            = string #allow only "LE_PROD" or "LE_STAGE"
+    dns_zone_name = string #dns zone name (used to grant permission to the dns zone for dns challenge)
+    dns_zone_rg   = string #dns zone resource group (used to scope permission to the dns zone only)
+    cn            = string #must be an fqdn format. Must include single quotes like so: "'myservice.mydomain.tld'"
+    sans          = string #can't be empty string
+    contact       = string #check valid email format
+  }))
+}
+
