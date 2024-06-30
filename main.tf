5b3a22ba4abd4f47bbecac1aab8a982eeab40257
Nate <nathanael.frappart@ryzhom.io>
2024-07-01 00:08:26 +0200

    fine tune variable validations && resource flags

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "azurerm_automation_account" "automation" {
     type = "SystemAssigned"
   }
 
-  sku_name = var.aa_sku #"Basic"
+  sku_name = var.aa_sku
 
   tags = var.aa_tags
 }
@@ -45,7 +45,7 @@ resource "azurerm_automation_schedule" "bimonthly" {
 
 resource "azurerm_automation_powershell72_module" "posh_acme" {
   name                  = "Posh-ACME"
-  automation_account_id = azurerm_automation_account.automation[0].id
+  automation_account_id = var.use_existing_aa == true ? data.azurerm_automation_account.automation[0].id : azurerm_automation_account.automation[0].id
 
   module_link {
     uri = "https://www.powershellgallery.com/api/v2/package/Posh-ACME/${var.posh_acme_version}"
@@ -69,57 +69,51 @@ resource "azurerm_key_vault" "kv" {
   purge_protection_enabled    = var.kv_purge_protection
 
   sku_name = var.kv_sku
+
+  enable_rbac_authorization = true
+}
+
+resource "azurerm_role_assignment" "kv" {
+  count                = var.use_existing_kv == true ? 0 : 1
+  scope                = azurerm_key_vault.kv[0].id
+  role_definition_name = "Key Vault Administrator"
+  principal_id         = data.azurerm_client_config.current.object_id
+
+  lifecycle {
+    ignore_changes = [
+      principal_id #to avoid intial role assignment to be rewritten when alternating between local run and CI run with different principals, changes are ignored
+    ]
+  }
 }
 
 resource "random_password" "posh_acme" {
-  count            = var.use_default_pwd == true ? 0 : 1
   length           = 16
   special          = true
   override_special = "@!-+[]"
 }
 
 resource "azurerm_key_vault_secret" "posh_acme" {
-  count        = var.use_default_pwd == true ? 0 : 1
   name         = "posh-acme"
-  value        = random_password.posh_acme[0].result
+  value        = random_password.posh_acme.result
   key_vault_id = var.use_existing_kv == true ? data.azurerm_key_vault.kv[0].id : azurerm_key_vault.kv[0].id
 
   lifecycle {
     ignore_changes = [value]
   }
+  depends_on = [ azurerm_role_assignment.kv ] #local.check_role_dependency
 }
-#resource "azurerm_automation_runbook" "letsencrypt" {
-#  name                    = "letsencrypt_www"
-#  resource_group_name = azurerm_resource_group.rg["rg-core"].name
-#  location            = azurerm_resource_group.rg["rg-core"].location
-#  automation_account_name = azurerm_automation_account.automation[0].name
-#  log_verbose             = "false" #"true"
-#  log_progress            = "true"
-#  description             = "Powershell script using posh-acme to generate letsencrypt certificates"
-#  runbook_type            = "PowerShell72"
-#
-#  content = file("${path.module}/assets/acme_www-dot-ryzhom-dot-com.ps1")#data.local_file.example.content
-#}
-#
-#
-#resource "azurerm_automation_job_schedule" "letsencrypt" {
-#  resource_group_name = azurerm_resource_group.rg["rg-core"].name
-#  automation_account_name = azurerm_automation_account.automation[0].name
-#  schedule_name           = azurerm_automation_schedule.bimonthly.name
-#  runbook_name            = azurerm_automation_runbook.letsencrypt.name
-#}
-
 
 resource "azurerm_automation_runbook" "acme_le" {
   for_each                = var.certs
   name                    = "letsencrypt_${each.key}"
   resource_group_name     = var.use_existing_rg == true ? data.azurerm_resource_group.automation[0].name : azurerm_resource_group.automation[0].name
   location                = var.location
-  automation_account_name = azurerm_automation_account.automation[0].name
-  log_verbose             = "false" #"true"
-  log_progress            = "true"
-  description             = "Powershell script using posh-acme to generate letsencrypt certificates"
-  runbook_type            = "PowerShell72"
+  automation_account_name = var.use_existing_aa == true ? data.azurerm_automation_account.automation[0].name : azurerm_automation_account.automation[0].name
+
+  log_verbose  = "false" #"true"
+  log_progress = "true"
+  description  = "Powershell script using posh-acme to generate letsencrypt certificates"
+  runbook_type = "PowerShell72"
 
   content = templatefile("${path.module}/assets/acme.ps1.tmpl", {
     subid       = data.azurerm_client_config.current.subscription_id,
@@ -141,6 +135,5 @@ resource "azurerm_role_assignment" "automation_dns" {
   for_each             = var.certs
   scope                = data.azurerm_dns_zone.automation_dns[each.key].id
   role_definition_name = "DNS Zone Contributor"
-  principal_id         = azurerm_automation_account.automation[0].identity[0].principal_id
+  principal_id         = var.use_existing_aa == true ? data.azurerm_automation_account.automation[0].identity[0].principal_id : azurerm_automation_account.automation[0].identity[0].principal_id
 }
-
--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "use_existing_rg" {
-  description = ""
+  description = "Flag to create resource group within module or Bring Your Own (BYO) resource group"
   type        = bool
   default     = false
 }
@@ -10,76 +10,93 @@ variable "aa_rg" {
 }
 
 variable "location" {
-  description = ""
+  description = "Azure region where the resources will be created"
   type        = string
 }
 
 variable "rg_tags" {
-  description = ""
+  description = "If creating resource group through module, use this variable to set tags"
   type        = map(any)
+  default     = {}
 }
 
 variable "use_existing_aa" {
-  description = ""
+  description = "Flag to create Automation Account within module or Bring Your Own (BYO) automation account"
   type        = bool
   default     = false
 }
 
 variable "aa_name" {
-  description = ""
+  description = "Automation Account name, whether it's an existing one (BYO) or created by the module"
   type        = string
 }
 
 variable "aa_sku" {
-  description = ""
+  description = "Sku for the Automation Account. Used only if Automation Account is created by module. Ignored if using BYO"
   type        = string
   default     = "Basic"
+
+  validation {
+    condition     = contains(["Free", "Basic"], var.aa_sku)
+    error_message = "The Automation Account sku can be either 'Free' or 'Basic'"
+  }
 }
 
 variable "aa_tags" {
-  description = ""
+  description = "Tags to assign to Automation Account."
   type        = map(string)
   default     = {}
 }
 
 variable "schedule_name" {
-  description = ""
+  description = "Name of the schedule used to execute runbook. Defaults to \"bimonthly\""
   type        = string
   default     = "bimonthly"
 }
 
 variable "schedule_timezone" {
-  description = ""
+  description = "Time zone to use for the schedule. Defaults to \"Europe/Paris\"."
   type        = string
   default     = "Europe/Paris"
 }
 
 variable "schedule_frequency" {
-  description = ""
+  description = "Frequency for the schedule. Defaults to \"Month\"."
   type        = string
   default     = "Month"
+
+  validation {
+    condition     = anytrue([ 
+      var.schedule_frequency == "Day",
+      var.schedule_frequency == "Hour",
+      var.schedule_frequency == "Month",
+      var.schedule_frequency == "Week",
+ 
+    ])
+    error_message = "Accepted values are \"Day\", \"Hour\", \"Month\", \"Week\"."
+  }
 }
 
 variable "schedule_interval" {
-  description = ""
+  description = "Specify interval for the selected freauency."
   type        = number
   default     = 2
 }
 
 variable "schedule_description" {
-  description = ""
+  description = "Brief description for the schedule to create for the runbook."
   type        = string
   default     = "Schedule to run every other month."
 }
 
 variable "posh_acme_version" {
-  description = ""
+  description = "Specify posh-acme version to install to automation account."
   type        = string
   default     = "4.22.0"
 }
 
 variable "use_existing_kv" {
-  description = ""
+  description = "Flag to create Key Vault within module or BYO Key Vault."
   type        = bool
   default     = false
 }
@@ -93,30 +110,39 @@ variable "kv_rg" {
   description = "Resource Group name of the existing Keyvault, if using one. This value will be ignored if Keyvault is created by the module (using var.aa_rg value instead)"
   type        = string
   default     = ""
+
+  validation {
+    condition     = var.use_existing_kv == true ? length(var.kv_rg) > 0 : true
+    error_message = "Empty string value is only valid when using an already existing Key Vault (i.e. when \"use_existing_kv\" variable is set to true )."
+  }
 }
 
 variable "kv_delete_retention" {
-  description = ""
+  description = "Set soft delete rentention policy (in days)"
   type        = number
   default     = 7
+
+  validation {
+    condition     = contains(range(7, 90), var.kv_delete_retention)
+    error_message = "Keyvault soft delete retention must be between 7 and 90 days"
+  }
 }
 
 variable "kv_purge_protection" {
-  description = ""
+  description = "Enable Keyvault purge protection"
   type        = bool
   default     = true
 }
 
 variable "kv_sku" {
-  description = ""
+  description = "Specify Key Vault sku."
   type        = string
   default     = "standard"
-}
 
-variable "use_default_pwd" {
-  description = ""
-  type        = bool
-  default     = true
+  validation {
+    condition     = contains(["standard", "premium"], var.kv_sku)
+    error_message = "Keyvault sku value can only be 'standard' or 'premium'"
+  }
 }
 
 variable "certs" {
@@ -129,5 +155,25 @@ variable "certs" {
     sans          = string #can't be empty string
     contact       = string #check valid email format
   }))
+
+  validation {
+    condition     = alltrue([ for k,v in var.certs : v.ca == "LE_STAGE" ]) || alltrue([ for k,v in var.certs : v.ca == "LE_PROD" ])
+    error_message = "CA can only be \"LE_STAGE\" or \"LE_PROD\"."
+  }
+
+  validation {
+    condition     = alltrue([ for k,v in var.certs : endswith(trim(v.cn, "'"), v.dns_zone_name) ])
+    error_message = "Certificate CN must match DNS Zone Name."
+  }
+
+  validation {
+    condition     = alltrue([ for k,v in var.certs : endswith(trim(v.sans, "'"), v.dns_zone_name) ])
+    error_message = "Certificate SANS must match DNS Zone Name."
+  }
+
+  validation {
+    condition     = alltrue([ for k,v in var.certs : startswith(v.cn, "'")]) && alltrue([ for k,v in var.certs : endswith(v.sans, "'")])
+    error_message = "Certificates SANS and CN must be between single quotes (e.g. \"'www.domain.tld'\")."  
+  }
 }
 
